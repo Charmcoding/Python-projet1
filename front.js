3a2e08c62131dddd12915c78730df90698421a1c
Charmcoding <155087098+Charmcoding@users.noreply.github.com>
2024-05-11 17:51:47 +0100

    Add files via upload

---
+++ front.js
@@ -0,0 +1,21 @@
+// pages/index.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to my Next.js App!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
+
+// components/Header.js
+import React from 'react';
+
+const Header = () => {
+  return <header>This is the header</header>;
+};
+
+export default Header;
